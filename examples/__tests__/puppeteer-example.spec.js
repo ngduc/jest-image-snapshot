c357e46f2b0279adedb6237a24f3e262e484c325
ducwings <ducjava@gmail.com>
2020-05-20 23:07:59 -0700

    feat(test): make "yarn test -u" works

--- examples/__tests__/puppeteer-example.spec.js
+++ examples/__tests__/puppeteer-example.spec.js
@@ -15,18 +15,22 @@
 // eslint is looking for `puppeteer` at root level package.json
 // eslint-disable-next-line import/no-unresolved
 const puppeteer = require('puppeteer');
+const path = require('path');
+
+const screenshotsPath = path.resolve(__dirname, '.');
 
 describe('jest-image-snapshot usage with an image received from puppeteer', () => {
   let browser;
 
   beforeAll(async () => {
-    browser = await puppeteer.launch();
+    const args = ['--no-sandbox', '--disable-setuid-sandbox', '--start-maximized', '--lang=he-IL', '--font-render-hinting=medium'];
+    browser = await puppeteer.launch({ headless: true, args });
   });
 
   it('works', async () => {
     const page = await browser.newPage();
-    await page.goto('https://example.com');
-    const image = await page.screenshot();
+    await page.goto('http://nothing.com');
+    const image = await page.screenshot({ path: `${screenshotsPath}/test.png` });
 
     expect(image).toMatchImageSnapshot();
   });
